d8952372fe6a9f41e3412ce4673e8f779c98bf44
Olajide-Raji <jyderaj@gmail.com>
2023-04-12 18:11:05 +0100

    origin

---
+++ arrays.js
@@ -0,0 +1,33 @@
+let myName = "sanmi";
+console.log(myName);
+let staffs = ["Yemi Eric", "Osato Moimoi", "Patrick Stevo", "David Isah", "Cash flow"];
+const grades = [20, 15, 40, 55, 26, 43, 45, 33, 44,55, 56, 44, 66, 23, 21 ];
+
+
+let people = [];
+people[0] = "shola";
+people[1] = "Khalid";
+people[2] = "Halimah";
+people[3] = "Olajide Raji";
+people[4] = "Ojomoyela Tola";
+people[5] = "Omotola Ojomoyela";
+
+
+var ss = grades[grades.lenght - 1];
+let stringsGrades = grades.toString();
+let tt = typeof stringsGrades;
+let ff = typeof grades;
+
+console.log(tt);
+console.log(ff);
+
+
+
+
+
+//staffs[5] = "James Tolu";
+//staffs.push("Musa Momoh");
+//console.log(grades);
+//console.log(grades);
+//grades.pop();
+//console.log(grades);
